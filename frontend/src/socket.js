a8a7a56a36c591d749e8b34d37d74312af86f1fc
Dhananjay Kumar <dhananjaykumar@Dhananjays-MacBook-Air.local>
2026-02-28 20:09:07 +0530

    Corrected socket URL to Render

--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -10,5 +10,5 @@ export const initSocket = async () => {
     
     // Dhyan de: Agar aapne server.js mein port 5001 kiya tha, toh yahan bhi 5001 hi likhna hai.
     // Agar 4000 kiya tha, toh 4000 likhna.
-    return io('http://localhost:5001', options);
+    return io('https://collaborative-editor-backend-d6lj.onrender.com', options);
 };
