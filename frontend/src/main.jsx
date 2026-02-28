e1c5d6f90b733dfc03818de1ffb61d4d670867d1
Dhananjay Kumar <dhananjaykumar@Dhananjays-MacBook-Air.local>
2026-02-28 22:43:12 +0530

    Step 4: Added timeout hack for hydration

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,8 +1,12 @@
-import { StrictMode } from 'react'
+import React from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-    <App />
-)
+const container = document.getElementById('root');
+const root = createRoot(container);
+
+// Ye 0 millisecond ka delay hydration error ko fix karne mein madad karta hai
+setTimeout(() => {
+    root.render(<App />);
+}, 0);
